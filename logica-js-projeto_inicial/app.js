d488ae1ccf137430dd8a9185d7d5bc7dc699d4c7
antonio <antonyejoao@gmail.com>
2025-01-17 18:31:02 -0300

    realizando primeira verificação por console baseado nos vídeos - testar sozinho a lógica antes de avançar

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert('Olá, imundo');
+
+let numeroSecreto = 2;
+let numeroChute = prompt('Digite um número entre 1 e 10')
+
+if(numeroChute == numeroSecreto) {
+    console.log('acertou')
+} else {
+    console.log('errou')
+}
