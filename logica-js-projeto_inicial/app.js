8d7754da2e33f6b932a8ff1c3f415e9b9cc8443e
antonio <antonyejoao@gmail.com>
2025-01-19 19:25:18 -0300

    subindo testes

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,15 @@
-alert('Olá, imundo');
-
 let numeroSecreto = 2;
-let numeroChute = prompt('Digite um número entre 1 e 10')
+let numeroChute;
 
-if(numeroChute == numeroSecreto) {
-    console.log('acertou')
-} else {
-    console.log('errou')
-}
+do {
+    numeroChute = parseInt(prompt('Digite um número entre 1 e 10'))
+
+    if(numeroChute > numeroSecreto) {
+        alert("você errou. tente mais para baixo")
+    } else if (numeroChute < numeroSecreto) {
+        alert("Você errou. tente mais para cima")
+    } else {
+        alert("isso mesmo! você acertou")
+    }
+
+} while (numeroChute != numeroSecreto)
